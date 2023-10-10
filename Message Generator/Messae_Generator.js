75c1f5ede22fef671bbf2d178927fefdd7ee4b52
Gibso10 <gibso.gwinn@gmail.com>
2023-10-10 10:34:53 -0500

    Adding message generator project

---
+++ Message Generator/Messae_Generator.js
@@ -0,0 +1,25 @@
+//Twister Instructions
+//ex outputs: right hand green, left foot yellow
+//three sets of data for 1.Left or right 2. hand or foot 3. red, blue, green , yellow
+
+//Start with my arrays of data
+// Left or Right
+arrayLR = ['Left', 'Right'];
+// Hand or Foot
+arrayHF = ['Hand', 'Foot'];
+//Color
+arrayC = ['Red', 'Blue', 'Green', 'Yellow'];
+
+//Create the function to run
+
+const twister = () => {
+    let lrRan = Math.floor(Math.random() * 2);
+    let hfRan = Math.floor(Math.random() * 2);
+    let cRan = Math.floor(Math.random() * 4);
+    let firstW = arrayLR[lrRan];
+    let secondW = arrayHF[hfRan];
+    let thirdW = arrayC[cRan];
+    return console.log(`${firstW} ${secondW} ${thirdW}`)
+}
+
+twister();
